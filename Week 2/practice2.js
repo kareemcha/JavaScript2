7c4d2434d7d4315eb2aba9c9057e6323b1cf713b
kareemcha <k.cha.esc@gmail.com>
2022-03-10 19:48:35 -0600

    Create practice2.js

---
+++ Week 2/practice2.js
@@ -0,0 +1,8 @@
+function convertFeetToInches (lengthft) {
+    console.log(lengthft*12) ;
+}
+
+    convertFeetToInches (1) ;
+    convertFeetToInches (3) ;
+    convertFeetToInches (30) ;
+    convertFeetToInches (30)
