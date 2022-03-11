9dce1b5f604b455450fd44ebf134118f4db0e18d
kareemcha <k.cha.esc@gmail.com>
2022-03-10 19:48:31 -0600

    Create hw2.js

---
+++ Week 2/hw2.js
@@ -0,0 +1,24 @@
+function sumOfSquareAreas(a, b) {
+    console.log((a*a)+(b*b)) ;
+} 
+        sumOfSquareAreas(9,3) ;
+
+function gradeCalculator(homeworkAvg, quizAvg, examAvg) {
+    console.log((homeworkAvg*.10) + (quizAvg*.15) + (examAvg*.75)) ;
+}
+        gradeCalculator(1,2,3) ; 
+        gradeCalculator(69, 2, 23) ;
+        gradeCalculator(50,100, 80) ;
+        gradeCalculator(2, 7, 8) ;
+        
+function sumOfSquareAreas(a, b) {
+    var sum = (a*a)+(b*b);
+    return sum;
+
+}
+
+var firstSquares = sumOfSquareAreas(2, 4);
+console.log(firstSquares);
+var seconfSquares = sumOfSquareAreas(3, 6);
+console.log(secondSquares)
+
